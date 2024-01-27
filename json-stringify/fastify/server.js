a147a9e7ea183d5e875b718b79cca1c471c5c19c
ivan <ivan.kavtarev@mail.ru>
2024-01-27 19:52:43 +0300

    add fastify

---
+++ json-stringify/fastify/server.js
@@ -0,0 +1,27 @@
+import Fastify from 'fastify'
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+async function run() {
+  const path = join('..', 'common', `${process.argv[2]}.json`);
+  const data = await readFile(path);
+  const json = JSON.parse(data);
+
+  const fastify = Fastify({})
+
+  // Declare a route
+  fastify.get('/', function (request, reply) {
+    reply.send(json)
+  })
+
+  // Run the server!
+  fastify.listen({ port: 3000 }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    // Server is now listening on ${address}
+  })
+}
+
+run()
