e7ae5158a5134e8511e785567e8da26b1e62e332
ivan <ivan.kavtarev@mail.ru>
2024-01-27 19:47:21 +0300

    add express

---
+++ json-stringify/express/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+
+const PORT = 3000;
+
+async function run() {
+  const path = join('..', 'common', `${process.argv[2]}.json`);
+  const data = await readFile(path);
+  const json = JSON.parse(data);
+
+  const app = express()
+
+  app.get('/', function (req, res) {
+    res.send(json)
+  })
+
+  app.listen(PORT, () => { console.log(`up on ${PORT}`); })
+}
+
+run()
