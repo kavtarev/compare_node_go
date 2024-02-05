bc07e2ad99840e94611ef12b339bc23e4d0bb376
ivan <ivan.kavtarev@mail.ru>
2024-02-05 10:12:34 +0300

    download files node

---
+++ common/files/README.md
@@ -0,0 +1,7 @@
+# tiny 1kb
+# small 1mb
+# medium 10mb
+# large 100mb
+
+# command 
+truncate -s 1M small.txt
--- node/server.js
+++ node/server.js
@@ -1,18 +1,25 @@
 import { createServer } from 'http';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
+import { createReadStream } from 'fs';
 
 const PORT = 3000;
 
 async function run() {
-  const path = join('..', 'common', 'json', `${process.argv[2]}.json`);
-  const data = await readFile(path);
-  const json = JSON.parse(data);
+  const jsonPath = join('..', 'common', 'json', `${process.argv[2]}.json`);
+  const jsonData = await readFile(jsonPath);
+  const json = JSON.parse(jsonData);
+
+  const filePath = join('..', 'common', 'files', `${process.argv[2]}.txt`);
 
   createServer((req, res) => {
-    if (req.url === 'json-stringify') {
+    if (req.url === '/json-stringify') {
       res.end(JSON.stringify(json));
     }
+    if (req.url === '/download-files') {
+      createReadStream(filePath).pipe(res);
+    }
+
   }).listen(PORT, () => { console.log(`up on ${PORT}`) })
 
 }
