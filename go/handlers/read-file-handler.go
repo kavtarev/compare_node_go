635bd286a0ded4371dbbd6d34516773c12498727
ivan <ivan.kavtarev@mail.ru>
2024-02-10 08:56:54 +0300

    rework read-file

---
+++ go/handlers/read-file-handler.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+	"io"
+	"fmt"
+)
+
+func ClosureReadFileHandlerChunk(name string) func (w http.ResponseWriter, req *http.Request) {
+	res := func (w http.ResponseWriter, r *http.Request) {
+		const size = 50000;
+	
+		f, err := os.Open("../common/files/" + name + ".txt")
+	
+		if err != nil {
+			panic("cant open file")
+		}
+		defer f.Close()
+	
+		bytes := make([]byte, size)
+	
+		for {
+			bytesRead, err := f.Read(bytes)
+	
+			if err != nil {
+				if err == io.EOF {
+					return
+				}
+				fmt.Println(err)
+			}
+	
+			w.Write(bytes[:bytesRead])
+		}
+	}
+
+	return res
+}
--- go/main.go
+++ go/main.go
@@ -1,58 +1,31 @@
 package main
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	// "compare/handlers"
+	"compare/handlers"
+	"compare/utils"
 )
 
 var myMap map[string]any
 
-func readFileHandlerChunk(w http.ResponseWriter, r *http.Request) {
-	const size = 50000;
-
-	f, err := os.Open("../common/files/small.txt")
-
-	if err != nil {
-		panic("cant open file")
-	}
-	defer f.Close()
-
-	bytes := make([]byte, size)
-
-	for {
-		bytesread, err := f.Read(bytes)
-
-		if err != nil {
-			if err == io.EOF {
-				return
-			}
-			fmt.Println(err)
-		}
-
-		w.Write(bytes[:bytesread])
-
-	}
-
-}
-
 func main() {
 		fmt.Println(os.Getpid())
-		// file := ReadJson(os.Args[1])
+		file := utils.ReadJson(os.Args[1])
 
-		// jErr := json.Unmarshal(file, &myMap)
+		jErr := json.Unmarshal(file, &myMap)
 
-		// if jErr != nil {
-		// 	panic(jErr)
-		// }
+		if jErr != nil {
+			panic(jErr)
+		}
 
-		// jsonHandler := handlers.ClosureJson(myMap)
+		jsonHandler := handlers.ClosureJson(myMap)
+		readFileHandler := handlers.ClosureReadFileHandlerChunk(os.Args[1])
 
-    // http.HandleFunc("/json-stringify", jsonHandler)
-    http.HandleFunc("/download-files", readFileHandlerChunk)
+    http.HandleFunc("/json-stringify", jsonHandler)
+    http.HandleFunc("/download-files", readFileHandler)
 
     http.ListenAndServe(":3000", nil)
 }
