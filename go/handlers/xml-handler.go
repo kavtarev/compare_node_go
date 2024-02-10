00ece03ea54255c6259d698f8ad1722c61accfa5
ivan <ivan.kavtarev@mail.ru>
2024-02-10 20:36:18 +0300

    add small struct

--- go/handlers/xml-handler.go
+++ go/handlers/xml-handler.go
@@ -13,7 +13,7 @@ func ClosureXmlHandler(name string) func(w http.ResponseWriter, r *http.Request)
 		panic("cant open xml file")
 	}
 
-	var obj TinyXml
+	var obj SmallXml
 
 	res := func(w http.ResponseWriter, r *http.Request) {
 		xml.Unmarshal(file, &obj)
@@ -23,12 +23,22 @@ func ClosureXmlHandler(name string) func(w http.ResponseWriter, r *http.Request)
 	return res
 }
 
-
-
 type TinyXml struct {
-
+	XMLName xml.Name `xml:"note"`
+	To string`xml:"to"`
+	From 	string `xml:"from"`
+	Heading 	string `xml:"heading"`
+	Body 	string `xml:"body"`
 }
 
 type SmallXml struct {
+	XMLName xml.Name `xml:"breakfast_menu"`
+	Food []Food `xml:"food"`
+}
 
+type Food struct {
+	Name string `xml:"name"`
+	Price string `xml:"price"`
+	Description string `xml:"description"`
+	Calories string `xml:"calories"`
 }
