3b23279157a26850239d747845f273c91f8b6f32
ivan <ivan.kavtarev@mail.ru>
2024-02-11 11:44:51 +0300

    node

--- node/server.js
+++ node/server.js
@@ -2,6 +2,7 @@ import { createServer } from 'http';
 import { readFile } from 'fs/promises';
 import { join } from 'path';
 import { createReadStream } from 'fs';
+import { XMLParser } from 'fast-xml-parser';
 
 const PORT = 3000;
 
@@ -12,12 +13,21 @@ async function run() {
 
   const filePath = join('..', 'common', 'files', `${process.argv[2]}.txt`);
 
+  const parser = new XMLParser();
+
+  const xmlPath = join('..', 'common', 'xml', `${process.argv[2]}.xml`);
+  const xmlData = await readFile(xmlPath);
+
   createServer((req, res) => {
     if (req.url === '/json-stringify') {
       res.end(JSON.stringify(json));
     }
     if (req.url === '/download-files') {
       createReadStream(filePath, { highWaterMark: 50000 }).pipe(res);
+    }
+    if (req.url === '/parse-xml') {
+      const xml = parser.parse(xmlData);
+      res.end(JSON.stringify(xml))
     }
 
   }).listen(PORT, () => { console.log(`up on ${PORT}`) })
